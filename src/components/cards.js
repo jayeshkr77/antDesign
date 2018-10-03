bebc6d1fdeb6591f532dd43977855d50122151c3
teqodoo <ravisk1514@gmail.com>
2018-10-03 21:27:58 +0530

    footer done

--- src/components/cards.js
+++ src/components/cards.js
@@ -11,11 +11,11 @@ class Cards extends Component {
     let top = 4;
 
     return (
-      <Card style={{marginBottom:'2px'}}>
+      <Card style={{ marginBottom: '2px' }}>
         <div style={{ margin: '-15px', marginBottom: '1px' }}>
           <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" width="100%" height="100%" />
         </div>
-        <h2>{this.props.title}</h2>
+        <h2 style={{ overflow: 'hidden' }}>{this.props.title}</h2>
         {this.props.genre && this.props.genre.map(gen => <div style={{ position: 'absolute', top: (top + (count++) * 28), left: -10, }}><Ribbon genre={gen} /></div>)}
 
       </Card>
